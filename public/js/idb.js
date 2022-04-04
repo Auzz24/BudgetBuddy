8b86fe17cff440e4e7c8949fadb925fdb49dc7cb
Austin Miller <austin.miller1424@gmail.com>
2022-04-04 11:17:57 -0500

    indexeddb added but not working

---
+++ public/js/idb.js
@@ -0,0 +1,56 @@
+let db;
+const request = indexedDB.open('BudgetBuddy', 1);
+
+request.onupgradeneeded = function(event){
+    const db = event.target.result;
+    db.createObjectStore('new_budget', {autoIncremant: true})
+}; 
+
+request.onsuccess = function (event) {
+    db= event.target.result;
+ if (navigator.onLine){
+     uploadBudget();
+ }
+};
+
+request.onerror = function (event){
+    console.log(event.target.errorCode);
+};
+
+function saveRecord (record){
+    const transaction =db.transaction(['new_budget'], 'readwrite');
+    const budgetObjectStore = transaction.objectStore('new_budget');
+    budgetObjectStore.add(record);
+}
+
+function uploadBudget() {
+    const transaction =db.transaction(['new_budget'], 'readwrite');
+    const budgetObjectStore = transaction.objectStore('new_budget');
+    const getAll = budgetObjectStore.getAll();
+    getAll.onsuccess = function (){
+        if (getAll.result.length >0){
+            fetch('/api/transaction', {
+                method: 'POST',
+                body: JSON.stringify(getAll.result),
+                headers:{
+                    accept: 'application/json, test/plain, */*',
+                    'Content-Type':'application/json'
+                }
+            })
+            .then(response => response.json())
+            .then(serverResponse => {
+              if (serverResponse.message) {
+                throw new Error(serverResponse);
+        }
+        const transaction =db.transaction(['new_budget'], 'readwrite');
+        const budgetObjectStore = transaction.objectStore('new_budget');
+        budgetObjectStore.clear();
+        alert('Budget was recorded');
+    })
+    .catch(err=>{
+        console.log(err);
+    });
+}
+}};
+
+window.addEventListener('online', uploadBudget);
